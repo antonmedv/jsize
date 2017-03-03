25f416d8329475dc75776122039e7447bd8f9832
Anton Medvedev <anton@medv.io>
2017-03-03 17:34:05 +0700

    Allow relative paths

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function npmInstall(packageName, callback) {
     npm.prefix = tmpDir;
     if (packageName) {
       npm.commands.install([packageName], function (err, deps) {
-        var location = deps[deps.length - 1][1]
-        var packagePath = path.join(path.dirname(tmpDir), location, 'package.json')
+        var location = deps[deps.length - 1][1];
+        var packagePath = path.join(path.dirname(tmpDir), location, 'package.json');
         packageName = require(packagePath).name;
         fs.writeFileSync(tmpIndexFilePath, 'require("' + packageName + '");');
         callback(packageName, tmpIndexFilePath);
