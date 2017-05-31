09ff4df13ec70b260a439f6bf3c44a2e05e9ff98
Dylan Piercey <pierceydylan@gmail.com>
2017-05-31 10:54:56 -0700

    Add process.browser to bundle

--- index.js
+++ index.js
@@ -61,7 +61,8 @@ function build() {
       plugins: [
         new webpack.optimize.UglifyJsPlugin({mangle: false, sourcemap: false}),
         new webpack.DefinePlugin({
-          'process.env.NODE_ENV': 'production'
+          'process.env.NODE_ENV': 'production',
+          'process.browser': true
         })
       ]
     }, (err, stats) => {
