4e0ace6f3d34f5ac45a417acbb65b2a0a7b7aa62
Anton Medvedev <anton@medv.io>
2017-09-02 14:04:08 +0700

    Add tests

---
+++ test.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const test = require('ava')
+const util = require('util')
+const jsize = require('./index')
+const execFile = util.promisify(require('child_process').execFile)
+
+test('programmatic usage', async t => {
+  const size = await jsize('react@15')
+  t.deepEqual(size, {
+    gzipped: 7231,
+    initial: 119470,
+    minified: 21614
+  })
+})
+
+test('programmatic usage +', async t => {
+  const size = await jsize(['react@15', 'react-dom@15'])
+  t.deepEqual(size, {
+    gzipped: 43614,
+    initial: 686503,
+    minified: 144762
+  })
+})
+
+test('cli usage', async t => {
+  const {stderr} = await execFile('bin/jsize', ['react@15'])
+  t.regex(stderr, /✔ react@15\s+7.23 kB \(gzipped\)/)
+})
+
+test('cli usage +', async t => {
+  const {stderr} = await execFile('bin/jsize', ['react@15+react-dom@15'])
+  t.regex(stderr, /✔ react@15 \+ react-dom@15\s+43.6 kB \(gzipped\)/)
+})
