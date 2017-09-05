5263f4044c2c9ced361739277fb83607cb876218
Dylan Piercey <pierceydylan@gmail.com>
2017-09-05 10:25:45 -0700

    Switch to babel-minify

--- test.js
+++ test.js
@@ -8,27 +8,27 @@ const execFile = util.promisify(require('child_process').execFile)
 test('programmatic usage', async t => {
   const size = await jsize('react@15')
   t.deepEqual(size, {
-    gzipped: 7231,
+    gzipped: 7383,
     initial: 119470,
-    minified: 21614
+    minified: 21869
   })
 })
 
 test('programmatic usage +', async t => {
   const size = await jsize(['react@15', 'react-dom@15'])
   t.deepEqual(size, {
-    gzipped: 43614,
+    gzipped: 44216,
     initial: 686503,
-    minified: 144762
+    minified: 146330
   })
 })
 
 test('cli usage', async t => {
   const {stdout} = await execFile('bin/jsize', ['react@15'])
-  t.regex(stdout, /react@15 *= *7\.23 kB \(gzipped\)/)
+  t.regex(stdout, /react@15 *= *7\.38 kB \(gzipped\)/)
 })
 
 test('cli usage +', async t => {
   const {stdout} = await execFile('bin/jsize', ['react@15+react-dom@15'])
-  t.regex(stdout, /react@15 \+ react-dom@15 *= *43\.6 kB \(gzipped\)/)
+  t.regex(stdout, /react@15 \+ react-dom@15 *= *44\.2 kB \(gzipped\)/)
 })
